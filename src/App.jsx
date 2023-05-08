8da4259f23005f0fce7b745416943c4b81ffe561
josephsantiagos <102764773+josephsantiagos@users.noreply.github.com>
2023-05-08 17:00:08 -0300

    sibir

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,71 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import { storage } from './firabase'
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 function App() {
-  const [imgURL, setimgURL] = useState("")
-  const [progress, setProgress] = useState("0")
+  const [imgURLs, setImgURLs] = useState([])
+  const [progress, setProgress] = useState(0)
+  const fileInputRef = useRef(null)
 
-  const handleupload = (event) => {
-    event.preventDefault();
-    const file = event.target[0]?.files[0]
-    if (!file) return;
+  const handleUpload = (event) => {
+    event.preventDefault()
+    const files = event.target[0]?.files
+    if (!files || files.length === 0) return
 
-    const storageRef = ref(storage, `imagens/${file.name}`)
-    const uploadTask = uploadBytesResumable(storageRef, file)
+    const promises = []
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i]
+      const storageRef = ref(storage, `imagens/${file.name}`)
+      const uploadTask = uploadBytesResumable(storageRef, file)
 
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        setProgress(progress)
-      },
-      error => { alert(error) },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then(url => {
-          setimgURL(url)
+      promises.push(
+        new Promise((resolve, reject) => {
+          uploadTask.on(
+            "state_changed",
+            snapshot => {
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+              setProgress(progress)
+            },
+            error => reject(error),
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then(url => {
+                resolve(url)
+              })
+            }
+          )
         })
-      }
-    )
+      )
+    }
+
+    Promise.all(promises).then(urls => {
+      setImgURLs([...imgURLs, ...urls])
+    }).catch(error => {
+      alert(error)
+    })
+  }
+
+  const handleClearSelection = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.value = null
+    }
   }
 
   return (
     <>
       <div>
         <header>
-          <form onSubmit={handleupload}>
-            <input type="file" />
+          <form onSubmit={handleUpload}>
+            <input type="file" multiple ref={fileInputRef} />
             <button type='submit'>enviar</button>
           </form>
+          <button onClick={handleClearSelection}>limpar seleção</button>
           <br />
           <br />
-          {!imgURL && <progress value={progress} max="100" />}
-          {imgURL && <img src={imgURL} alt="imagem" />}
+          {imgURLs.length === 0 && <progress value={progress} max="100" />}
+          {imgURLs.map(url => (
+            <img key={url} src={url} alt="imagem" />
+          ))}
         </header>
       </div>
     </>
